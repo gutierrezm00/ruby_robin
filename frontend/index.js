d3828bb21341a741b16c4f3de11527080350b85b
Mathew <mathewgutierrez00@gmail.com>
2020-09-10 14:26:19 -0600

    work on frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './src/components/App';
+import App from './src/components';
 
 const appRoot = document.getElementById('app');
 
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Graph from '../js/graph.js'
+import Graph from '../js/graph'
 
 export default function Default() {
 	return (
